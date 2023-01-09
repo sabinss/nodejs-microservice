363e75570027e092d4b4ab5110f0969eac355113
sabin <sabin@noveltytechnology.com>
2023-01-09 08:35:32 +0545

    first commit

---
+++ shopping_ms/gateway/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/customers', proxy('http://localhost:8004'));
+app.use('/products', proxy('http://localhost:8003'));
+app.use('/', proxy('http://localhost:8002')); //shopping
+
+app.listen(8000, () => {
+  console.log('Gateway is Listening to Port 8000');
+});
